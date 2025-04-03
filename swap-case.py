07ac98149dd8539b0867a64a1cc6bcf453f65049
rienngine <renan.cstbnss@gmail.com>
2025-04-02 21:56:20 -0300

    swapcase() implementation

--- swap-case.py
+++ swap-case.py
@@ -1,12 +1,26 @@
+# Convert all lowercase characters in a string to uppercase, and all uppercase characters to lowercase.
+# Example: Input "HeLLo" -> Output "hEllO".
+
+''' (First try)
 def swap_case(s):
-    for char in s:
-            if char.islower():
-                s = s.replace(char, char.upper())
-            elif char.isupper():
-                s = s.replace(char, char.lower())     
+    for c in range(len(s)):
+            if s[c].isupper():
+                s = s[:c] + s[c].lower() + s[c+1:]
+            elif s[c].islower():
+                s = s[:c] + s[c].upper() + s[c+1:]
     return s
 
 if __name__ == '__main__':
     s = input()
     result = swap_case(s)
     print(result)
+'''
+
+# For this specific case, simply use swapcase() method.
+
+def swap_case(s):
+    return s.swapcase()
+
+s = input()
+result = swap_case(s)
+print(result)
